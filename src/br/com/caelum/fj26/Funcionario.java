53917337604579ad49f1adcda806ba3251be13e3
soliveira.vagner <soliveira.vagner@gmail.com>
2011-09-13 20:02:50 +0000

    Criada a classe Funcionario.

---
+++ src/br/com/caelum/fj26/Funcionario.java
@@ -0,0 +1,65 @@
+package br.com.caelum.fj26;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Classe que representa a entidade Funcionario
+ * 
+ * @author vagner
+ * 
+ */
+@Entity
+public class Funcionario {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	private Long id;
+	private String nome;
+	private String usuario;
+	private String senha;
+	private String email;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
