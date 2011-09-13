75e0b8b2b1f17715ce4f1f419bd512b1decbfff8
soliveira.vagner@gmail.com <soliveira.vagner@gmail.com@3ec9f806-8ae6-0d05-1b9b-207bef88f060>
2011-09-13 21:28:00 +0000

    Alterado o valor da propriedade strategy, pois estava estava incrementando o id de 20 em 20.

--- trunk/src/br/com/caelum/fj26/Funcionario.java
+++ trunk/src/br/com/caelum/fj26/Funcionario.java
@@ -15,7 +15,7 @@ import javax.persistence.Id;
 public class Funcionario {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 	private String nome;
 	private String usuario;
