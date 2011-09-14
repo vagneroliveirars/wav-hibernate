9ec0dd38601ed07ed5adf74d1bcdde1ddcf481cc
soliveira.vagner <soliveira.vagner@gmail.com>
2011-09-14 20:41:07 +0000

    Criadas e mapeadas as classes ContaPagar e Fornecedor

---
+++ src/br/com/caelum/fj26/Fornecedor.java
@@ -0,0 +1,51 @@
+package br.com.caelum.fj26;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+/**
+ * Classe que representa a entidade Fornecedor
+ * 
+ * @author vagner
+ * 
+ */
+@Entity
+public class Fornecedor {
+
+	@Id
+	@SequenceGenerator(name = "FORNECEDOR_ID", sequenceName = "FORNECEDOR_SEQ", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "FORNECEDOR_ID")
+	@Column(name = "ID")
+	private Long id;
+	private String nome;
+	private String descrição;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescrição() {
+		return descrição;
+	}
+
+	public void setDescrição(String descrição) {
+		this.descrição = descrição;
+	}
+
+}
