5b0d7a9476940bf72fb3bc8f22c8d7e167be4a9a
soliveira.vagner <soliveira.vagner@gmail.com>
2011-09-14 18:05:44 +0000

    Criada e mapeada a classe Produto.

---
+++ src/br/com/caelum/fj26/Produto.java
@@ -0,0 +1,36 @@
+package br.com.caelum.fj26;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+public class Produto {
+
+	@Id
+	@SequenceGenerator(name = "PRODUTO_ID", sequenceName = "PRODUTO_SEQ", allocationSize = 1)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "PRODUTO_ID")
+	@Column(name = "ID")
+	private Long id;
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
