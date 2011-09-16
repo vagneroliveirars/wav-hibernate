16d2f23c2fde5c22da2bd6a164bd78bdc7020c6c
soliveira.vagner@gmail.com <soliveira.vagner@gmail.com@3ec9f806-8ae6-0d05-1b9b-207bef88f060>
2011-09-16 20:40:30 +0000

    Criada a classe Dao

---
+++ trunk/src/br/com/caelum/fj26/Dao.java
@@ -0,0 +1,10 @@
+package br.com.caelum.fj26;
+
+import java.util.logging.Logger;
+
+public class Dao<T> {
+	
+	private static Logger logger = Logger.getLogger(Dao.class.getName());
+	private Class persistentClass;
+
+}
