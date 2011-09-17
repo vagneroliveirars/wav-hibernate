d943ec00b44ddc47883e793e1e94094fa2cbe2b2
soliveira.vagner <soliveira.vagner@gmail.com>
2011-09-17 02:05:07 +0000

    Operações básicas adicionadas

--- src/br/com/caelum/fj26/Dao.java
+++ src/br/com/caelum/fj26/Dao.java
@@ -2,9 +2,40 @@ package br.com.caelum.fj26;
 
 import java.util.logging.Logger;
 
+import org.hibernate.Session;
+
+/**
+ * Classe DAO generica que serve para qualquer entidade e que fornece as
+ * operacoes mais basicas
+ * 
+ * @author vagner
+ * 
+ * @param <T>
+ */
 public class Dao<T> {
-	
+
 	private static Logger logger = Logger.getLogger(Dao.class.getName());
 	private Class persistentClass;
+	private Session session;
+
+	public Dao(Session session, Class persistentClass) {
+		this.session = session;
+		this.persistentClass = persistentClass;
+	}
+
+	@SuppressWarnings("unchecked")
+	public T load(Long id) {
+		logger.info("lendo " + persistentClass + " com id " + id);
+		return (T) session.load(persistentClass, id);
+	}
+
+	public void save(T t) {
+		logger.info("salvando " + t);
+		session.save(t);
+	}
+
+	protected Session getSession() {
+		return session;
+	}
 
 }
