35f712a9f23d129de2a73f2c496f9bb6efea0485
soliveira.vagner@gmail.com <soliveira.vagner@gmail.com@3ec9f806-8ae6-0d05-1b9b-207bef88f060>
2011-09-22 21:08:43 +0000

    Novos métodos e javadoc adicionados

--- trunk/src/br/com/caelum/fj26/FornecedorDao.java
+++ trunk/src/br/com/caelum/fj26/FornecedorDao.java
@@ -1,5 +1,8 @@
 package br.com.caelum.fj26;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 
 /**
@@ -12,6 +15,21 @@ public class FornecedorDao extends Dao<Fornecedor> {
 
 	public FornecedorDao(Session session) {
 		super(session, Fornecedor.class);
+	}
+
+	/**
+	 * Busca um fornecedor com determinado nome. Nao e Case Sensitive
+	 * 
+	 * @param nome
+	 * @return lista de fornecedores
+	 */
+	@SuppressWarnings("unchecked")
+	public List<Fornecedor> buscaFornecedorPorNome(String nome) {
+		Query q = getSession().createQuery("select f from " 
+				+ Fornecedor.class.getName()
+				+ " as f where lower(f.nome) like lower(:nome)");
+		q.setParameter("nome", "%" + nome + "%");
+		return q.list();
 	}
 
 }
