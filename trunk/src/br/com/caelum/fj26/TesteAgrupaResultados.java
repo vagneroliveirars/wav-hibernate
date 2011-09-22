d98c0bb68194144db750c4ad083feeef7fb76543
soliveira.vagner@gmail.com <soliveira.vagner@gmail.com@3ec9f806-8ae6-0d05-1b9b-207bef88f060>
2011-09-22 18:47:11 +0000

    Criada a classe TesteAgrupaResultados

---
+++ trunk/src/br/com/caelum/fj26/TesteAgrupaResultados.java
@@ -0,0 +1,28 @@
+package br.com.caelum.fj26;
+
+import java.util.List;
+
+import org.hibernate.Session;
+
+public class TesteAgrupaResultados {
+
+	public static void main(String[] args) {
+		Session session = HibernateUtil.openSession();
+		ContaPagarDao contaPagarDao = new ContaPagarDao(session);
+		List<Object[]> lista = contaPagarDao.listaFornecedorValor(10);
+		for (Object[] fv : lista) {
+			Fornecedor f = (Fornecedor) fv[0];
+			String situacao;
+			if ((Boolean) fv[1]) {
+				situacao = "Pagas";				
+			} else {
+				situacao = "Não pagas";
+			}
+			System.out.println(f.getNome() + " - "
+					+ f.getDescricao() 
+					+ " - " + situacao);
+			System.out.printf("%.2f", fv[2]);
+			System.out.println();
+		}
+	}
+}
