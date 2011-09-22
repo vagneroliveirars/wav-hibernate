903503452229c541fad2ea48904a46868d0f0f57
soliveira.vagner@gmail.com <soliveira.vagner@gmail.com@3ec9f806-8ae6-0d05-1b9b-207bef88f060>
2011-09-22 00:34:09 +0000

    Novos métodos implementados

--- trunk/src/br/com/caelum/fj26/ContaPagarDao.java
+++ trunk/src/br/com/caelum/fj26/ContaPagarDao.java
@@ -1,6 +1,8 @@
 package br.com.caelum.fj26;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.Projections;
 
 /**
  * Classe DAO especifica da entidade ContaPagar
@@ -12,6 +14,20 @@ public class ContaPagarDao extends Dao<ContaPagar> {
 
 	public ContaPagarDao(Session session) {
 		super(session, ContaPagar.class);
+	}
+	
+	// calcula a media do valor
+	public double avgValor() {
+		Criteria c = getSession().createCriteria(ContaPagar.class);
+		c.setProjection(Projections.avg("valor"));
+		return (Double) c.uniqueResult();
+	}
+	
+	// calcula a soma do valor
+	public double sumValor() {
+		Criteria c = getSession().createCriteria(ContaPagar.class);
+		c.setProjection(Projections.sum("valor"));
+		return (Double) c.uniqueResult();
 	}
 
 }
