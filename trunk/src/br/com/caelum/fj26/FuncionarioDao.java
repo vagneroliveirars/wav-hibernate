774945a8d5b25368f6aa438df02eabd7e1587c30
soliveira.vagner@gmail.com <soliveira.vagner@gmail.com@3ec9f806-8ae6-0d05-1b9b-207bef88f060>
2011-09-22 00:28:54 +0000

    Novos métodos implementados

--- trunk/src/br/com/caelum/fj26/FuncionarioDao.java
+++ trunk/src/br/com/caelum/fj26/FuncionarioDao.java
@@ -2,7 +2,12 @@ package br.com.caelum.fj26;
 
 import java.util.List;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
 
 /**
  * Classe DAO especifica da entidade Funcionario
@@ -17,13 +22,35 @@ public class FuncionarioDao extends Dao<Funcionario> {
 	}
 
 	public int countTotalFuncionarios() {
-		// logica para calcular quantas linhas tem no BD
-		return 0;
+		Criteria c = getSession().createCriteria(Funcionario.class);
+		c.setProjection(Projections.rowCount());
+		return (Integer) c.uniqueResult();
 	}
 
+	@SuppressWarnings("unchecked")
 	public List<Funcionario> buscaFuncionarioPorNome(String nome) {
-		// logica para buscar pelo nome
-		return null;
+		Criteria c = getSession().createCriteria(Funcionario.class);
+		c.add(Restrictions.ilike("nome", nome));
+		c.addOrder(Order.asc("email"));
+		return c.list();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Funcionario> buscaFuncionarioPorNomeEEmail(String nome, String email) {
+		Criteria c = getSession().createCriteria(Funcionario.class);
+		c.add(Restrictions.ilike("nome", nome));
+		c.add(Restrictions.ilike("email", email));
+		return c.list();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Funcionario> buscaFuncionarioPorNomeOuEmail(String nome, String email) {
+		Criteria c = getSession().createCriteria(Funcionario.class);
+		Criterion c1 = Restrictions.ilike("nome", nome);
+		Criterion c2 = Restrictions.ilike("email", email);
+		Criterion c3 = Restrictions.or(c1, c2);
+		c.add(c3);
+		return c.list();
 	}
 
 }
